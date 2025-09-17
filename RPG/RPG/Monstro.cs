3d4a20d8590789a50d57b7a0d39f1e4e5233203b
Pietro Santos <pietro.oliveirabr@gmail.com>
2025-09-17 00:35:32 -0300

    Add files via upload

---
+++ RPG/RPG/Monstro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG
+{
+    //heranca e emcapsulamento
+    public class Monstro : Entidade 
+    {
+        public string Tipo { get; set; }
+        public Monstro(string nome, int pontosDeVida, int ataque, string tipo) : base(nome, pontosDeVida, ataque)
+        {
+            Tipo = tipo;
+        }
+        
+        //polimorfismo
+        public override void ReceberDano(int dano)
+        {
+            base.ReceberDano(dano);
+            if (EstaVivo())
+                Console.WriteLine($"{Nome} rosna com raiva!");
+            else
+                Console.WriteLine($"{Nome} foi derrotado!");
+        }
+    }
+}
---
+++ RPG/RPG/Program.cs
@@ -0,0 +1,18 @@
+﻿using RPG;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Personagem heroi = new Personagem("Arthorias", 100, 20, "Guerreiro");
+        Monstro monstro = new Monstro("Manus", 200, 20, "Pai do Abismo");
+
+        Batalha batalha = new Batalha();
+        batalha.Iniciar(heroi, monstro);
+
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+
+    }
+}
